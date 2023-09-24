2d0b6078f5bc44659d1c69d23a33df8e60ba3ac7
srideviselvam <145249658+srideviselvam@users.noreply.github.com>
2023-09-24 10:58:52 +0530

    challenge2.1.py

--- challenge2.1.py
+++ challenge2.1.py
@@ -1,14 +1,10 @@
-# 1.1 implement a recursive function to calculate the foctorial of a given nubmer
-
-
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
-
-
-number = int(input("ENTER A VALUE: "))
-res = fact_rec(number)
-
-print("The factorial of {} is {}.".format(number, res))
+class playar():
+  def play(self):
+    print("the player is playing crickter.")
+    
+class Batsman (Player):
+  def play (self):
+    print("The Bastman is batting.")
+    
+ batsman=batsman()
+ batsman.play()
