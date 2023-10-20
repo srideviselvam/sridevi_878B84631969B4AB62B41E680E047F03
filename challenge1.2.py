a4976ef54fe2116129fb0738170d02fb3b572c13
srideviselvam <145249658+srideviselvam@users.noreply.github.com>
2023-10-20 11:37:57 +0530

    Update challenge1.2.py

--- challenge1.2.py
+++ challenge1.2.py
@@ -1,14 +1,21 @@
-# 1.1 implement a recursive function to calculate the foctorial of a given nubmer
+leap year
 
-def fact_rec(n):
-   if n==0 or n==1:
-     return 1
-   else:
-     return n*fact_rec(n-1)
 
-number = int(input("ENTER A VALUE: "))
-res=fact_rec(number) 
+  year % 4 == 0 &
+year % 100 != 0 /
+year % 400 == 0
 
-print("The factorial of {} is {}.".format(number,res))
+"""
+def isLeapYear(year):
+  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True
+  else:
+    return False
 
-  
+year = int(input("Enter a year : "))
+
+if isLeapYear(year):
+  print('{} is a leap year.'.format(year))
+else:
+  print('{} is not a leap year.'.format(year))
+    
